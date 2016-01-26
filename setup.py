989a6dfb7171cf5b23c6e059dc348d5bd1cede13
bibi21000 <bibi21000@gmail.com>
2016-01-26 18:51:43 +0100

    update setup

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ setup(
                     ],
     dependency_links = [
       'https://github.com/bibi21000/janitoo/archive/master.zip#egg=janitoo',
-      'https://github.com/bibi21000/janitoo_raspberry_i2c/archive/master.zip#egg=janitoo_rapsberry_i2c',
+      'https://github.com/bibi21000/janitoo_raspberry_i2c/archive/master.zip#egg=janitoo_raspberry_i2c',
       'https://github.com/adafruit/Adafruit_Python_GPIO/archive/master.zip#egg=Adafruit-GPIO',
       'https://github.com/adafruit/Adafruit-Motor-HAT-Python-Library/archive/master.zip#egg=Adafruit_MotorHAT',
     ],
