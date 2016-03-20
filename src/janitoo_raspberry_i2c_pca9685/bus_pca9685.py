78dadd3f9e7aa599e9086b641cfcc989ee170856
bibi21000 <bibi21000@gmail.com>
2016-03-21 00:09:42 +0100

    Auto-commit

--- src/janitoo_raspberry_i2c_pca9685/bus_pca9685.py
+++ src/janitoo_raspberry_i2c_pca9685/bus_pca9685.py
@@ -48,6 +48,7 @@ from janitoo_raspberry_i2c_pca9685.thread_pca9685 import OID
 
 try:
     from Adafruit_MotorHAT.Adafruit_PWM_Servo_Driver import PWM
+    from Adafruit_MotorHAT import Adafruit_MotorHAT
 except:
     logger.exception("Can't import Adafruit_MotorHAT")
 
@@ -89,14 +90,14 @@ class Pca9685Bus(JNTBus):
     def start(self, mqttc, trigger_thread_reload_cb=None):
         JNTBus.start(self, mqttc, trigger_thread_reload_cb)
         try:
-            self.pca9685 = PWM(address=self.values["%s_hexadd"%OID].data)
+            self.pca9685 = Pca9685Manager(address=self.values["%s_hexadd"%OID].data)
         except:
             logger.exception('Exception when intialising pca9685 board')
         self.update_attrs('pca9685', self.pca9685)
 
     def stop(self):
         if self.pca9685 is not None:
-            self.pca9685.softwareReset()
+            self.pca9685.software_reset()
         self.pca9685 = None
         self.update_attrs('pca9685', self.pca9685)
         JNTBus.stop(self)
@@ -106,3 +107,55 @@ class Pca9685Bus(JNTBus):
 
         """
         return self.pca9685 is not None
+
+class Pca9685Manager(Adafruit_MotorHAT):
+    """To share bus with the adafruit library
+    """
+
+    def __init__(self, addr = 0x40, freq = 1600):
+        """Init
+        """
+        self._i2caddr = addr        # default addr
+        self._frequency = freq      # default @1600Hz PWM freq
+        self._motors = []
+        self._steppers = []
+        self._leds = []
+        self._pwm =  PWM(addr, debug=False)
+        self._pwm.setPWMFreq(self._frequency)
+
+    @property
+    def pwm(self):
+        """
+        """
+        return self._pwm
+
+    @property
+    def motors(self):
+        """
+        """
+        if self._motors is None:
+            self._motors = [ Adafruit_DCMotor(self, m) for m in range(4) ]
+        return self._motors
+
+    @property
+    def steppers(self):
+        """
+        """
+        if self._steppers is None:
+            self._steppers = [ Adafruit_StepperMotor(self, 1), Adafruit_StepperMotor(self, 2) ]
+        return self._steppers
+
+    def software_reset(self):
+        """
+        """
+        self._pwm.softwareReset()
+
+    def setPwm(self, pin, value_on):
+        """
+        """
+        if (pin < 0) or (pin > 15):
+            raise NameError('PWM pin must be between 0 and 15 inclusive')
+        if (value_on < 0) and (value_on > 4096):
+            raise NameError('Pin value must be between 0 and 4096!')
+        value_off = 4096 - value_on
+        self._pwm.setPWM(pin, value_on, value_off)
