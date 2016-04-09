c4471d2b6b0a2b3b0b89052b531dfbd434408c75
bibi21000 <bibi21000@gmail.com>
2016-04-09 17:38:30 +0200

    Update tests for docker

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -51,8 +51,9 @@ class TestPiSerser(JNTTDockerServer, JNTTDockerServerCommon):
     broker_user = 'toto'
     broker_password = 'toto'
     server_class = PiServer
-    server_conf = "tests/data/janitoo_raspberry_pca9865.conf"
+    server_conf = "tests/data/janitoo_raspberry_i2c_pca9865.conf"
     hadds = [HADD%(140,0), HADD%(140,1), HADD%(140,2) ]
 
     def test_040_server_start_no_error_in_log(self):
+        JNTTDockerServer.onlyDockerTest()
         JNTTDockerServerCommon.minimal_040_server_start_reload_restart(self)
