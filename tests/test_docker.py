f4a2c40d35e79583bd9d9575475310391cecaabd
bibi21000 <bibi21000@gmail.com>
2016-04-12 01:10:35 +0200

    Some updates for docker

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -43,7 +43,7 @@ from janitoo.utils import HADD_SEP, HADD
 
 from janitoo_raspberry.server import PiServer
 
-class TestPiSerser(JNTTDockerServer, JNTTDockerServerCommon):
+class TestRaspberryPCA9685Serser(JNTTDockerServer, JNTTDockerServerCommon):
     """Test the server
     """
     loglevel = logging.DEBUG
