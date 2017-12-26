b5152a536598af87fa0dee11363fbbdd4e579060
bibi21000 <bibi21000@gmail.com>
2017-12-26 22:12:23 +0100

    Fix import

--- tests/test_pca9685.py
+++ tests/test_pca9685.py
@@ -43,7 +43,7 @@ from janitoo.utils import TOPIC_HEARTBEAT
 from janitoo.utils import TOPIC_NODES, TOPIC_NODES_REPLY, TOPIC_NODES_REQUEST
 from janitoo.utils import TOPIC_BROADCAST_REPLY, TOPIC_BROADCAST_REQUEST
 from janitoo.utils import TOPIC_VALUES_USER, TOPIC_VALUES_CONFIG, TOPIC_VALUES_SYSTEM, TOPIC_VALUES_BASIC
-from janitoo.compat umport next
+from janitoo.compat import next
 
 import janitoo_raspberry_i2c_pca9685.bus_pca9685
 import janitoo_raspberry_i2c_pca9685.pca9685
